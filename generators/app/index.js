6613020b30b9118b83de6b59056abb19c21f0d99
Michi Kono <michikono@gmail.com>
2015-07-19 00:27:52 -0700

    added prefix to all directives (optional)

--- generators/app/index.js
+++ generators/app/index.js
@@ -37,6 +37,12 @@ module.exports = helpers.Base.extend({
         store: true
       },
       {
+        type: 'input',
+        name: 'directivePrefix',
+        message: 'Prefix added to all directives (see Y073 in style guide); enter nothing/space to skip this',
+        store: true
+      },
+      {
         type: 'list',
         name: 'router',
         message: 'Which router would you like to use?',
@@ -53,10 +59,11 @@ module.exports = helpers.Base.extend({
 
     this.prompt(prompts, function (choices) {
       this.choices = choices;
-      this.choices.appName = changeCase.camelCase(this.choices.appName);
-      this.choices.clientSideFolder = changeCase.paramCase(this.choices.clientSideFolder).replace(/(.*?)\/$/, '$1') + '/';
+      this.choices.appName = changeCase.camelCase(this.choices.appName.trim());
+      this.choices.clientSideFolder = changeCase.paramCase(this.choices.clientSideFolder.trim()).replace(/(.*?)\/$/, '$1') + '/';
       this.choices.clientSideFolderMinusSlash = this.choices.clientSideFolder.slice(0, -1);
-      this.choices.appSubFolder = changeCase.paramCase(this.choices.appSubFolder).replace(/(.*?)\/$/, '$1') + '/';
+      this.choices.directivePrefix = changeCase.camelCase(this.choices.directivePrefix.trim());
+      this.choices.appSubFolder = changeCase.paramCase(this.choices.appSubFolder.trim()).replace(/(.*?)\/$/, '$1') + '/';
       if (choices.router === 'Angular UI Router') {
         this.choices.uirouter = true;
         this.choices.routerJs = 'bower_components/angular-ui-router/release/angular-ui-router.js';
@@ -72,6 +79,7 @@ module.exports = helpers.Base.extend({
       this.config.set('appName', this.choices.appName);
       this.config.set('clientSideFolder', this.choices.clientSideFolder);
       this.config.set('appSubFolder', this.choices.appSubFolder);
+      this.config.set('directivePrefix', this.choices.directivePrefix);
       this.config.set('uirouter', this.choices.uirouter);
 
       done();
