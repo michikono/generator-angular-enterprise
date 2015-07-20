39854e26183ed0414cc5cd21bcf84bbb32ba7dd3
Michi Kono <michikono@gmail.com>
2015-07-19 18:02:33 -0700

    updated documentation in main module

--- generators/app/templates/client/app/_app.module.js
+++ generators/app/templates/client/app/_app.module.js
@@ -8,7 +8,9 @@
   'use strict';
 
   /**
-   * Thin app module
+   * Thin app module; place global dependencies you install in <%= appName %>.core; this should only contain
+   * feature modules + <%= appName %>.core. Each sub-module (core included) needs to maintain its own list
+   * of dependencies
    *   https://github.com/johnpapa/angular-styleguide#style-y161
    *   https://github.com/johnpapa/angular-styleguide#style-y162
    */
