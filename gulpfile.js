acdeca74c3d662a162f51031f560552942d19243
John Katsnelson <voltalocke@gmail.com>
2015-07-21 18:59:37 -0700

    adds broken watch command

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var gutil = require('gulp-util');
 
-gulp.task('test', function() {
-  return gulp.src('test/*.js', {read: false})
-    .pipe(mocha({reporter: 'nyan'}));
+gulp.task('mocha', function() {
+  return gulp.src(['test/*.js'], { read: false })
+    .pipe(mocha({ reporter: 'nyan' }))
+    .on('error', gutil.log);
+});
+
+gulp.task('watch-mocha', function() {
+  gulp.watch(['generators/**', 'test/**'], ['mocha']);
 });
