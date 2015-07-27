ab5392ac83fe39189ea82e9382e28fb5a8e3e110
Michi Kono <michikono@gmail.com>
2015-07-26 20:28:54 -0700

    added alias information

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require('gulp');
 var mocha = require('gulp-spawn-mocha');
 var gutil = require('gulp-util');
 
+// aliases
+gulp.task('test', ['mocha']);
+gulp.task('watch', ['watch-mocha']);
+
 gulp.task('mocha', function() {
   return gulp.src(['test/*.js'], { read: false })
     .pipe(mocha({ reporter: 'nyan' }))
@@ -11,3 +15,4 @@ gulp.task('mocha', function() {
 gulp.task('watch-mocha', function() {
   gulp.watch(['generators/**', 'test/**'], ['mocha']);
 });
+
