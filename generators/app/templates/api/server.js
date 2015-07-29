0ac463b81219327f21e8546fef8ebac2f31077d7
Forrest Thomas <forrestbthomas@gmail.com>
2015-07-28 23:08:19 -0700

    include michis suggestions

--- generators/app/templates/api/server.js
+++ generators/app/templates/api/server.js
@@ -1,6 +1,6 @@
 process.env.DEBUG = 'swagger:*';
 var swaggerServer = require('swagger-server');
-var app = swaggerServer('api/DemoSwagger.yaml');
+var app = swaggerServer('api/swagger-demo.yaml');
 app.listen(3002, function() {
 	console.log('swagger is running');
 });
