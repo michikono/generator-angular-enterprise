5f03bd57859ccd18e551e8530b1ea562b0e21173
Forrest Thomas <forrestbthomas@gmail.com>
2015-08-04 09:13:01 -0700

    add test for decorator injection

---
+++ test/fixtures/decorator.js
@@ -0,0 +1,20 @@
+/**
+ * Separate configuration file for modules
+ *   https://github.com/johnpapa/angular-styleguide#style-y128
+ */
+(function () {
+  'use strict';
+
+  angular.module('app').config(config);
+
+  /* @ngInject */
+  function config($provide) {
+
+  	function logDecorator($delegate) {
+  	  return {};
+  	};
+
+  	$provide.decorator("log", logDecorator);
+  	// [INJECT:DECORATORS] Generated decorators appear here, at this indent level. DO NOT REMOVE. 
+  }
+})();
